bcdd0f319c0c5d887f6ad00d4d43b3bbc102e521
63070028 <71110346+63070028@users.noreply.github.com>
2020-12-13 16:15:56 +0700

    Full-docsting

--- Gacha.py
+++ Gacha.py
@@ -7,12 +7,15 @@ box, item, rate_box, sr_box, sr_rate, ssr_box, ssr_rate, grade = [], [], [], [],
 
 
 def clears():
-    text_label.destroy()
-    button_get['state'] = NORMAL
+    """function clear""" #ฟังก์ชันที่ใช้ในการลบข้อความที่แสดงการรับค่าและเปิดปุ่ม get เพื่อป้องกันการกดที่ผิดพลาด
+    text_label.destroy() #ลบข้อความที่แสดงผลว่ารับค่าแล้ว
+    button_get['state'] = NORMAL #เปิดปุ่ม Get
 
 
 def get():
+    """function get value""" #เป็นฟังก์ชันที่ใช้ในการเก็บค่าที่รับมาเพื่อใช้ในการคำวนวณ
     global text_label
+
     tier = tier_t.get()
     if tier == 1:
         tier = "R"
@@ -24,13 +27,13 @@ def get():
         tier = "None"
     elif tier == 0:
         messagebox.showerror("Error Tier", "Please select tier")
-    
+
     if item_t.get() == "":
         messagebox.showerror("Error Item", "Please enter item")
 
     if tier != 0 and item_t.get() != "":
-        text_label = Label(root, text="Save    Item:  %s      Tier:  %s       Rate:  %s"%(item_t.get(), tier, rate_t.get()), font = 'dubai 12 bold')
-        print("รับค่า    Item:  %s      Tier:  %s       Rate:  %s"%(item_t.get(), tier, rate_t.get()))
+        text_label = Label(root, text="Save    Item:  %s      Tier:  %s       Rate:  %s"%(item_t.get(), tier, rate_t.get()), font = 'dubai 12 bold', fg = 'salmon')
+        print("Get values    Item:  %s      Tier:  %s       Rate:  %s"%(item_t.get(), tier, rate_t.get()))
 
         box.append(item_t.get())
         rate_box.append(float(rate_t.get()))
@@ -44,31 +47,32 @@ def get():
                         ssr_box.append(item_t.get())
                         ssr_rate.append(float(rate_t.get()))
 
-        item_t.delete(0, END)
-        rate_t.delete(0, END)
-
+        item_t.delete(0, END) #ลบส่วนที่กรอบใน item
+        rate_t.delete(0, END) #ลบส่วนที่กรอบใน rate
         text_label.pack()
 
-        sampling_get['state'] = NORMAL
-        button_get['state'] = DISABLED
+        sampling_get['state'] = NORMAL #เปิดปุ่ม Sampling
+        button_get['state'] = DISABLED #ปิดปุ่ม Get ป้องกันการกดผิด
 
 
 def sampling_option():
+    """Sampling Option""" #ใช้ในการเลือกรูปแบบของการ sampling
     global garuntee_button
     global normal_button
-    normal_button = Button(root, text=' Normal  ', font = 'dubai 10 bold', command=sampling_n, bg = 'oldlace')
+    normal_button = Button(root, text=' Normal  ', font = 'dubai 10 bold', command=sampling_n, bg = 'oldlace', fg = 'salmon', width=10)
     normal_button.pack()
-    garuntee_button = Button(root, text='Garuntee', font = 'dubai 10 bold', command=sampling_g, bg = 'tomato')
+    garuntee_button = Button(root, text='Garuntee', font = 'dubai 10 bold', command=sampling_g, bg = 'tomato', fg = 'snow', width=10)
     garuntee_button.pack()
 
 
 def sampling_n():
-    garuntee_button['state'] = DISABLED
-    Label(root, text="สุ่มแบบ Normal", font = 'dubai 10 bold').pack()
+    """Normal Sampling""" #ฟังก์ชัน การสุ่มแบบทั่วไป โดยไม่มีเงื่อนไขมาเกี่ยวข้อง
+    garuntee_button['state'] = DISABLED #ปิดปุ่ม garuntee
+    Label(root, text="Normal Random", font = 'dubai 10 bold', fg = 'salmon').pack()
 
     def sampling_calculate_n():
+        """Normal calculate"""
         get = []
-
 
         roll_all = int(roll_t.get())
 
@@ -79,6 +83,8 @@ def sampling_n():
 
         for _ in range(1, roll_all+1):
             get.append(choices(box, weights=rate_box, k=1))
+
+
         for unit in get:
             if unit not in keeper:
                 keeper.append(unit)
@@ -90,26 +96,26 @@ def sampling_n():
             count = 0
         keeper = [i[0] for i in keeper]
         for l in range(len(keeper)):
-            print("ได้รับ     %s"%keeper[l], end = " ")
-            print("จำนวน     %s"%printer[l]+' ชิ้น')
+            print("Get   %s"%keeper[l], end = " ")
+            print("  %s"%printer[l]+'  piece')
 
 
-
-    roll_label = Label(root, text="กี่ Roll", font = 'dubai 10 bold')
-    roll_label.pack()
+    Label(root, text="How many roll?", font = 'dubai 10 bold', fg = 'salmon').pack()
     roll_t = Entry(root)
     roll_t.pack()
 
-
-    normal_submit = Button(root, text='Submit', font = 'dubai 10 bold', command=sampling_calculate_n,  bg = 'tomato')
+    normal_submit = Button(root, text='Submit', font = 'dubai 10 bold', command=sampling_calculate_n,  bg = 'tomato', fg = 'snow', width=10)
     normal_submit.pack()
 
 
 def sampling_g():
-    normal_button['state'] = DISABLED
-    Label(root, text="สุ่มแบบมี Garuntee", font = 'dubai 10 bold').pack()
+    """Garuntee Sampling""" #ฟังก์ชัน การสุ่มแบบมีเงื่อนไขมาเกี่ยวข้อง โดยจะเป็น รูปแบบ R, SR, SSR โดยการสุ่มทุก 10 ครั้งจะรับประกัน SR หรือ SSR 1 อัน
+    normal_button['state'] = DISABLED #ปิดปุ่ม ์normal
+    Label(root, text="Garuntee Random", font = 'dubai 10 bold').pack()
+
 
     def sampling_calculate_g():
+        """Garuntee calculate"""
         if len(sr_box) == 0 :
             messagebox.showerror("Error SR", "Please enter SR")
         elif len(ssr_box) == 0:
@@ -121,15 +127,16 @@ def sampling_g():
             garuntee_option = int(garuntee_option_t.get())
             roll_all = int(roll_t.get())
 
-
             count = 0
             keeper = []
             printer = []
 
+            roll_garuntee = 10 #การสุ่มทุก 10 ครั้งจะรับประกัน SR หรือ SSR 1 อัน สามารถปรับเปลี่ยนได้
+
 
             for roll in range(1, roll_all+1):
                 garuntee += 1
-                if roll % 10 != 0:
+                if roll % roll_garuntee != 0:
                     get.append(choices(box, weights=rate_box, k=1))
                     if choices(box, weights=rate_box, k=1) in ssr_box: #ถ้าได้ SSR การันตีจะรีกับไปที่ 0
                         garuntee = 0
@@ -154,20 +161,16 @@ def sampling_g():
                 count = 0
             keeper = [i[0] for i in keeper]
             for l in range(len(keeper)):
-                print("ได้รับ     %s"%keeper[l], end = " ")
-                print("จำนวน     %s"%printer[l]+' ชิ้น')
+                print("Get   %s"%keeper[l], end = " ")
+                print("  %s"%printer[l]+'  piece')
 
 
 
-    garuntee_option_label = Label(root, text="Garuntee Option(ประกัน)", font = 'dubai 10 bold')
-    garuntee_option_label.pack()
+    Label(root, text="Garuntee Option", font = 'dubai 10 bold').pack()
     garuntee_option_t = Entry(root)
     garuntee_option_t.pack()
 
-
-
-    roll_label = Label(root, text="กี่ Roll", font = 'dubai 10 bold')
-    roll_label.pack()
+    Label(root, text="How many roll?", font = 'dubai 10 bold').pack()
     roll_t = Entry(root)
     roll_t.pack()
 
@@ -175,50 +178,53 @@ def sampling_g():
     garuntee_submit.pack()
 
 
-
+#ส่วนที่ใช้ในการตั้งชื่อและปรับขนาด
 root = Tk()
-root.title("Gacha_simulater")
+root.title("Gacha_simulator")
 root.geometry("500x800")
 
 
-item_label = Label(root, text="Item", font = 'dubai 10 bold')
-item_label.pack()
+#ส่วนของการกรอก Item
+Label(root, text="Item", font = 'dubai 12 bold', fg = 'salmon').pack()
 item_t = Entry(root, width=40)
 item_t.pack()
 
 
-rate_label = Label(root, text="Rate", font = 'dubai 10 bold')
-rate_label.pack()
+#่ส่วนของการกรอก Rate
+Label(root, text="Rate", font = 'dubai 12 bold', fg = 'salmon').pack()
 rate_t = Entry(root, width=40)
 rate_t.pack()
 
 
+#ส่วนของการเลือก Tier ถ้าเราต้องการสุ่มแบบทั่วไปโดยไม่มีเงื่อนไข ให้เลือก None
 tier_t = IntVar()
 
-tier_label = Label(root, text="Tier", font = 'dubai 10 bold')
-tier_label.pack()
-Radiobutton(root, text='    R    ', variable=tier_t, value=1).pack()
-Radiobutton(root, text='   SR   ', variable=tier_t, value=2).pack()
-Radiobutton(root, text='  SSR  ', variable=tier_t, value=3).pack()
-Radiobutton(root, text='None ', variable=tier_t, value=4).pack()
+Label(root, text="Tier", font = 'dubai 12 bold', fg = 'salmon').pack()
+Radiobutton(root, text='    R    ', variable=tier_t, value=1, font = 'dubai 10 bold', fg = 'salmon').pack()
+Radiobutton(root, text='   SR   ', variable=tier_t, value=2, font = 'dubai 10 bold', fg = 'salmon').pack()
+Radiobutton(root, text='  SSR  ', variable=tier_t, value=3, font = 'dubai 10 bold', fg = 'salmon').pack()
+Radiobutton(root, text='None ', variable=tier_t, value=4, font = 'dubai 10 bold', fg = 'salmon').pack()
 
 
 Label(root, text=" ").pack()
 
-button_get = Button(root, text=' Get  ', font = 'dubai 10 bold', command = get, bg = 'tomato')
+
+#ปุ่ม Get
+button_get = Button(root, text=' Get  ', font = 'dubai 10 bold', command = get, bg = 'tomato', fg = 'snow', width=10)
 button_get.pack()
 
-button_clear = Button(root, text='Clear', font = 'dubai 10 bold', command = clears, bg = 'oldlace')
+#ปุ่ม Clear
+button_clear = Button(root, text='Clear', font = 'dubai 10 bold', command = clears, bg = 'oldlace', width=10, fg = 'salmon')
 button_clear.pack()
 
 
 Label(root, text=" ").pack()
 
-sampling_get = Button(root, text='Sampling', font = 'dubai 10 bold', command = sampling_option, bg = 'tomato')
+
+#ปุ่ม Sampling
+sampling_get = Button(root, text='Sampling', font = 'dubai 10 bold', command = sampling_option, bg = 'tomato', fg = 'snow', width=10)
 sampling_get['state'] = DISABLED
 sampling_get.pack()
-
-
 
 
 root.mainloop()
